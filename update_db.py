9ae9b6cd61827faaa56d6de04dc0b2663d0d42d7
lng205 <lng205.yb@gmail.com>
2024-02-24 15:32:42 +0800

    simplify code

--- update_db.py
+++ update_db.py
@@ -2,11 +2,6 @@ from scripts.update import update_ids, update_posts
 from app import app
 
 
-def main():
-    with app.app_context():
-        update_ids()
-        update_posts()
-
-
-if __name__ == "__main__":
-    main()
+with app.app_context():
+    update_ids()
+    update_posts()
